2f1adec1aeb934a3455325b0b9993faa3c2e1096
Oliver Scherf-Clavel <oliver.wahl@uni-wuerzburg.de>
2019-11-03 16:20:58 +0000

    update nlmixr example

--- popPK_201.R
+++ popPK_201.R
@@ -3,6 +3,10 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
+library(nlmixr)
+library(xpose)
+library(xpose.nlmixr)
+
 mod <- mread("Examplotinib")
 
 set.seed(16111983)
@@ -17,23 +21,13 @@ plot(res)
 
 dat <- as.data.frame(res)
 
-ggplot(dat) + geom_point(aes(x=time, y=DV, colour=as.factor(SEX) )) + facet_wrap(dat$ID)
 
 
-sample_dat <- filter(dat, dat$time %in% c(0,0.25, 0.5, 0.75, 1, 1.25, 2, 2.5, 4, 6, 8, 12, 24))
+sample_dat <- filter(dat, dat$time %in% c(0, 0.5, 0.75, 1, 2, 2.5, 4, 6, 8, 12, 24))
 
+del_col <- which(colnames(sample_dat) %in% c("GUT", "CENT", "PER", "KA_ind", "CL_ind", "V_ind", "KE_ind", "VP_ind", "Q_ind", "IPRED"))
 
-
-ggplot(sample_dat) + geom_point(aes(x=time, y=DV)) + facet_wrap(sample_dat$ID)
-
-library(writexl) 
-
-write_xlsx(sample_dat, path="./pat_dat.xlsx")
-
-library(readxl)
-
-data <- read_excel("./pat_dat.xlsx")
-
+data <- sample_dat[,-del_col]
 
 data$cmt <- 2
 
@@ -51,13 +45,18 @@ data <- data[-which(data$time==0 & data$evid==0),]
 
 write.csv(data, "pat_dat.csv", row.names = F)
 
-data <- read.csv("pat_dat.csv")
-
-d <- nmDataConvert(data);
+ggplot(data, aes(x=time, y=DV)) + geom_point() + facet_wrap(data$ID)
 
 
 
+ggplot(data, aes(x=time, y=DV, color=as.factor(ID) )) +
+  geom_point() +
+  geom_line()
 
+ggplot(data, aes(x=time, y=DV)) +
+  geom_point() +
+  stat_smooth() +
+  scale_y_log10()
 
 my2CptModel <- function() {
   ini({
@@ -66,13 +65,11 @@ my2CptModel <- function() {
     tv2  <- log(35)
     tv3  <- log(450)
     tq   <- log(5)
-    tlagtime <- log(0.5)
     eta.ka ~ 1
     eta.cl ~ 1
     eta.v2 ~ 1
     eta.v3 ~ 1
     eta.q ~ 1
-    eta.tlag ~1
     add.err <- 0.075
   })
   model({
@@ -81,7 +78,6 @@ my2CptModel <- function() {
     v2 <- exp(tv2 + eta.v2)
     v3 <- exp(tv3 + eta.v3)
     q  <- exp(tq + eta.q)
-    tlag <- exp(tlagtime+eta.tlag)
     d/dt(depot) = -ka * depot
     d/dt(center) = ka * depot - cl / v2 * center + q/v3 * periph - q/v2 * center
     d/dt(periph) = q/v2 * center - q/v3 * periph
@@ -91,88 +87,27 @@ my2CptModel <- function() {
 }
 
 
-library(nlmixr)
-library(ggplot2)
-
-
-
-ggplot(d) + geom_point(aes(x=TIME, y=DV)) + facet_wrap(d$ID)
-
-ggplot(d) + geom_point(aes(x=TIME, y=LNDV)) + facet_wrap(d$ID)
-
-my2CptFit <- nlmixr(my2CptModel, d, est="saem", control=saemControl(nBurn = 200, nEm=300))
+my2CptFit <- nlmixr(my2CptModel, data, est="saem", 
+                    control=saemControl(nBurn = 200, nEm=300),
+                    table=tableControl(cwres=TRUE, npde=TRUE))
 
 plot(my2CptFit)
 
-ggplot(my2CptFit) + geom_point(aes(x=TIME, y=DV, colour=ID)) + 
-  geom_line(aes(x=TIME, y=IPRED, colour=ID)) + facet_wrap(my2CptFit$ID)
 
 xpdb <- xpose.nlmixr::xpose_data_nlmixr(my2CptFit)
 
-library(xpose.nlmixr)
-library(xpose)
-
-dv_vs_ipred(xpdb)
-
-xpose::eta_distrib(xpdb)
 
 
-
-library(nlmixr)
-library(ggplot2)
-library(xpose.nlmixr)
-#-----------------------------------------------------------
-#Exploratory plots
-
-df = Theoph
-
-ggplot(df, aes(x=Time, y=conc, color=Subject)) +
-  geom_point() +
-  geom_line()
-
-ggplot(df, aes(x=Time, y=conc)) +
-  geom_point() +
-  stat_smooth() +
-  scale_y_log10()
-
-#------------------------------------------------------------
-# Theophylline model using linCmt
-m1 <- function() {
-  ini({
-    tka <- .5
-    tcl <- -3.2
-    tv <- -1
-    eta.ka ~ 1
-    eta.cl ~ 2
-    eta.v ~ 1
-    add.err <- 0.1
-  })
-  model({
-    ka <- exp(tka + eta.ka)
-    cl <- exp(tcl + eta.cl)
-    v <- exp(tv + eta.v)
-    linCmt() ~ add(add.err)
-  })
-}
-
-fit1 <- nlmixr(m1, theo_sd, est="saem", table=tableControl(cwres=TRUE, npde=TRUE))
-
-print(fit1)
-
-#fit1 <- fit1 %>% addCwres() # In case this was not specified under model fit prior to estimation one could add this here to the results
-
-#GoF by xpose
-xpdb <- xpose_data_nlmixr(fit1)
 
 dv_vs_pred(xpdb) +
-  ylab("Observed Theophylline Concentrations (ng/mL)") +
-  xlab("Population Predicted Theophylline Concentrations (ng/mL)")
+  ylab("Observed Examplotinib Concentrations (mg/L)") +
+  xlab("Population Predicted Examplotinib Concentrations (mg/L)")
 dv_vs_ipred(xpdb) +
-  ylab("Observed Theophylline Concentrations (ug/mL)") +
-  xlab("Individual Predicted Theophylline Concentrations (ng/mL)")
+  ylab("Observed Examplotinib Concentrations (mg/L)") +
+  xlab("Individual Predicted Examplotinib Concentrations (mg/L)")
 res_vs_pred(xpdb) +
   ylab("Conditional Weighted Residuals") +
-  xlab("Population Predicted Theophylline Concentrations (ng/mL)")
+  xlab("Population Predicted Examplotinib Concentrations (mg/L)")
 res_vs_idv(xpdb) +
   ylab("Conditional Weighted Residuals") +
   xlab("Time (h)")
@@ -182,12 +117,12 @@ absval_res_vs_idv(xpdb, res = 'IWRES') +
   xlab("Time (h)")
 absval_res_vs_pred(xpdb, res = 'IWRES')  +
   ylab("Individual Weighted Residuals") +
-  xlab("Population Predicted Theophylline Concentrations (ng/mL)")
+  xlab("Population Predicted Examplotinib Concentrations (mg/L)")
 ind_plots(xpdb, nrow=3, ncol=4) +
-  ylab("Predicted and Observed Theophylline concentrations (ng/mL)") +
+  ylab("Predicted and Observed Examplotinib concentrations (mg/L)") +
   xlab("Time (h)")
 res_distrib(xpdb) +
   ylab("Density") +
   xlab("Conditional Weighted Residuals")
-nlmixr::vpc(fit1,nsim=500, show=list(obs_dv=T),
-            ylab = "Theophylline Concentrations (ng/mL)", xlab = "Time (h)")
+nlmixr::vpc(my2CptFit,nsim=500, show=list(obs_dv=T),
+            ylab = "Examplotinib Concentrations (mg/L)", xlab = "Time (h)")
